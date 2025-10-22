2636696073d10c024d915901aff33df9c05f1b18
5KFubi <5kelefubi@gmail.com>
2025-10-22 19:20:38 +0300

    Test 2

--- src/main/java/me/fivekfubi/api/BaseRaidersProvider.java
+++ src/main/java/me/fivekfubi/api/BaseRaidersProvider.java
@@ -1,5 +1,11 @@
 package me.fivekfubi.api;
 
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
 public interface BaseRaidersProvider {
     void test();
+
+    List<String> get_player_protection_nicks_test(Player player);
 }
