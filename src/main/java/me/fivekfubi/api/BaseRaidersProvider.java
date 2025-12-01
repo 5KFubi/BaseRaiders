dd0417dbeac3da7cf866769d426cf4ade5fe93b7
5KFubi <5kelefubi@gmail.com>
2025-12-01 16:22:48 +0200

    2.0.4

--- src/main/java/me/fivekfubi/api/BaseRaidersProvider.java
+++ src/main/java/me/fivekfubi/api/BaseRaidersProvider.java
@@ -1,9 +1,11 @@
 package me.fivekfubi.api;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public interface BaseRaidersProvider {
     void test();
@@ -11,4 +13,8 @@ public interface BaseRaidersProvider {
     List<String> get_player_protection_nicks_test(Player player);
 
     Map<String, I_DATA_Protection> get_protection_database_cache();
+
+    boolean has_permission(Player player, Location event_location, String permission);
+    boolean has_permission(UUID uuid, Location event_location, String permission);
+    boolean has_permission(String uuid_string, Location event_location, String permission);
 }
